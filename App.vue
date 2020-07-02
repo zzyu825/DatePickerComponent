80086f2592b10cdcb560ae18f1f98a6ff42086b9
zhengzhiyu <zhengzhiyu825@163.com>
2020-07-02 16:08:19 +0800

    html

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <date-picker />
+    </div>
+</template>
+
+<script>
+import datePicker from './DatePicker'
+
+export default {
+    components: {
+        datePicker
+    }
+}
+</script>
---
+++ DatePicker.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="date-picker">
+        <!-- 输入区域 -->
+        <div class="picker-input">
+            <span class="input-prefix">
+                <i></i>
+            </span>
+            <input type="text">
+        </div>
+        <!-- 选择区域 -->
+        <div class="picker-panel">
+            <div class="picker-arrow" />
+            <div class="picker-body">
+                <div class="picker-header">
+                    <span class="picker-btn icon-prev-year"></span>
+                    <span class="picker-btn icon-prev-month"></span>
+                    <span class="picker-date">
+                        2020年7月
+                    </span>
+                    <span class="picker-btn icon-next-month"></span>
+                    <span class="picker-btn icon-next-year"></span>
+                </div>
+                <div class="picker-content">
+                    <div class="picker-weeks">
+                        <div 
+                            v-for="week of ['日', '一', '二', '三', '四', '五', '六']"
+                            :key="week"
+                        >{{ week }}</div>
+                    </div>
+                    <div class="picker-days">
+                        <div 
+                            v-for="date of 42"
+                            :key="date"
+                        >{{ date }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
